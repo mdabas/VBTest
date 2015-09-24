423dcc8b7a61cee57ecaeafdf969a4e76b4fd73d
mdabas <mdabas@infinite-usa.com>
2015-09-24 21:43:38 +0530

    Manish did this

---
+++ MVCwithVisaulBasic/Controllers/HelloWorldController.vb
@@ -0,0 +1,19 @@
+﻿Imports System.Web.Mvc
+
+Namespace Controllers
+    Public Class HelloWorldController
+        Inherits Controller
+
+        ' GET: HelloWorld
+        Function Index() As ActionResult
+            Return View()
+        End Function
+
+        Function Welcome(name As String, Optional numtimes As Integer = 1, Optional ID As Integer = 1) As ActionResult
+            ViewBag.Message = "Hello " & name
+            ViewBag.NumTimes = numtimes
+            Return View()
+            'Return HttpUtility.HtmlEncode("Hello " & name & "; Num Times is : " & numtimes & " ID :" & ID)
+        End Function
+    End Class
+End Namespace
---
+++ MVCwithVisaulBasic/Views/HelloWorld/Welcome.vbhtml
@@ -0,0 +1,12 @@
+﻿@Code
+    ViewBag.Title = "Welcome"
+End Code
+<h2>Welcome</h2>
+
+<ul>
+    @For i = 1 To ViewBag.NumTimes
+        @<li>@ViewBag.Message : cheers</li> ' Manish added cheers - Sep24, 2015
+        @<li>@i times</li> 
+
+    Next
+</ul>
